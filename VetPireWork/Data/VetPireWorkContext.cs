ea1e731cf583bb1e0b523ada000932a865154381
Gabriel <gabriel_pires111@outlook.com>
2021-05-25 15:32:50 -0300

    criacao de crud scaffolding

---
+++ VetPireWork/Data/VetPireWorkContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VetPireWork.Models;
+
+namespace VetPireWork.Data
+{
+    public class VetPireWorkContext : DbContext
+    {
+        public VetPireWorkContext (DbContextOptions<VetPireWorkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VetPireWork.Models.Department> Department { get; set; }
+    }
+}
--- VetPireWork/Startup.cs
+++ VetPireWork/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using VetPireWork.Data;
 
 namespace VetPireWork
 {
@@ -33,6 +35,9 @@ namespace VetPireWork
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<VetPireWorkContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("VetPireWorkContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
