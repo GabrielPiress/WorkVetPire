cfb0796bbe238095e6207695d4d904df2a10fe94
Gabriel <gabriel_pires111@outlook.com>
2021-05-25 11:09:59 -0300

    primeiro commit

---
+++ VetPireWork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VetPireWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
